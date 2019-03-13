a0b465fc708fb4ea05d023b725404c247e3d4a6d
Jonathan Flowers <jmflowers134@gmail.com>
2019-03-12 21:11:07 -0400

    Update importer.py

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -6,7 +6,7 @@ def import_gists_to_database(db, username, commit=True):
     response = requests.get(url)
     list_of_gists = response.json()
     query = """INSERT INTO gists (github_id,html_url,git_pull_url,git_push_url,commits_url,
-                forks_url,public,created_at,updated_at,comments,updated_at,comments,comments_url) 
+                forks_url,public,created_at,updated_at,comments,comments_url) 
                 VALUES (?,?,?,?,?,?,?,?,?,?,?)"""
     for gist in list_of_gists:
         db.execute(query, [gist['id'],gist['html_url'],
